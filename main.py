31a32a49a17c71b2807632303341039b460f262d
Papricie <patriciehermanova@gmail.com>
2025-08-25 15:08:34 +0200

    pylance

--- main.py
+++ main.py
@@ -17,8 +17,21 @@ oddelovac = "-" * 50
 
 ################################### KÓD #######################################
 
+# IMPORT KNIHOVEN
 import sys
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
+# VALIDACE ARGUMENTŮ
+def validate_args(args):
+    if len(args) != 3:
+        print("Chyba: Musíš zadat 2 argumenty (URL a CSV soubor).")
+        return False
+    url = args[1]
+    if not url.startswith("http"):
+        print("Chyba: První argument musí být platný URL odkaz.")
+        return False
+    return True
 
+# STÁHNUTÍ STRÁNKY
